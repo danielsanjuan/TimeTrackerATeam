0a27086689e6352315e4d82727d7e41a1abd60bc
Alejandro Padron <alejandro.padron@edosof.es>
2017-11-20 14:52:08 +0000

    Home Fronted

---
+++ frontEnd/src/app/check/check.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { NgClass } from '@angular/common'; 
+
+@Component({
+  selector: 'app-check',
+  templateUrl: './check.component.html',
+  styleUrls: ['./check.component.css']
+})
+export class CheckComponent implements OnInit {
+  checkInTime: string;
+  checkOutTime: string;
+  doClick:boolean = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  timeCheckIn(){
+    this.checkInTime = moment().format('YYYY/MM/DD, hh:mm:ss');
+    this.doClick=false;
+    console.log(this.checkInTime);
+  }
+
+  timeCheckOut(){
+    this.checkOutTime = moment().format('YYYY/MM/DD, hh:mm:ss');
+    this.doClick=true;
+    console.log(this.checkOutTime);
+  }
+  }
+}
