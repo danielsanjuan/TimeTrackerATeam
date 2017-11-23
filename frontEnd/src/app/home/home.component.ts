88fc67a8427ce0d39fbccfdb098613b2f0e334be
Alejandro Padron <alejandro.padron@edosof.es>
2017-11-23 16:48:08 +0000

    Modified different between TimeCheckIn and TimeNow

--- frontEnd/src/app/home/home.component.ts
+++ frontEnd/src/app/home/home.component.ts
@@ -12,9 +12,8 @@ export class HomeComponent implements OnInit {
 
   today:string = moment().format('MMM Do YYYY, h:mm a');
   hours:string = moment().format('HH:mm');
+  fechaway:any = moment().format('YYYY-MM-DD HH:mm:ss.x');
   hours_today:string;
-  checkInTime:string;
-
 
   constructor(private services:CheckInService) { }
 
@@ -23,45 +22,30 @@ export class HomeComponent implements OnInit {
 
   time(){
     this.services.getCheckIn().subscribe((data)=>{
-      console.log(data);
-      this.checkInTime = data.response_date;
-      this.timeToday(this.checkInTime, this.hours);
+      let fechaCheckIn = new Date(data.response_date);
+      let fechaNow = new Date(this.fechaway);
+      this.workDayTime(fechaCheckIn, fechaNow);
     });
   }
 
-  timeToday(dateT, dateN){
-        let fechaT:string = dateT.split("");
-        let fechaN:string = dateN.split("");
-        let horasT = fechaT[11];
-        horasT += fechaT[12];
-        let minT = fechaT[14];
-        minT += fechaT[15];
-        let horasN = fechaN[0];
-        horasN += fechaN[1];
-        let minN = fechaN[3];
-        minN += fechaN[4];
-        let hora_checkIn = parseInt(horasT);
-        let min_checkIn = parseInt(minT);
-        let hora_now = parseInt(horasN);
-        let min_now = parseInt(minN);
-        let dif_hour = hora_now - hora_checkIn;
-        let dif_min = min_now - min_checkIn;
-        if(dif_min<0){
-          dif_hour -= 1;
-          dif_min += 60;
-        }
-        if(dif_hour<10 && dif_min<10){
-          this.hours_today = "0"+dif_hour.toString()+":0"+dif_min.toString();
-        }
-        if(dif_hour<10 && dif_min>10){
-          this.hours_today = "0"+dif_hour.toString()+":"+dif_min.toString();
-        }
-        if(dif_hour>10 && dif_min<10){
-          this.hours_today = dif_hour.toString()+":0"+dif_min.toString();
-        }
-        if(dif_hour>10 && dif_min>10){
-          this.hours_today = dif_hour.toString()+":"+dif_min.toString();
-        }
-     }
+  workDayTime(dateCheck, dateNow){
+      let hours = ((dateNow - dateCheck)/3600000);
+      let minutes = ((hours*60)%60);
+      alert(minutes);
+      alert(hours.toFixed(0));
+      if(hours<10 && minutes<10){
+        this.hours_today = "0"+hours.toFixed(0)+":0"+minutes.toFixed(0);
+      }
+      if(hours<10 && minutes>10){
+        this.hours_today = "0"+hours.toFixed(0)+":"+minutes.toFixed(0);
+      }
+      if(hours>10 && minutes<10){
+        this.hours_today = hours.toFixed(0)+":0"+minutes.toFixed(0);
+      }
+      if(hours>10 && minutes>10){
+        this.hours_today = hours.toFixed(0)+":"+minutes.toFixed(0);
+      }
+      alert(this.hours_today);
+  }
 
 }
