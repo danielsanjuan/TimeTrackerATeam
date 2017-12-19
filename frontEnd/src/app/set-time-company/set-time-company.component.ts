a0f12390f04d83955d0bfa9a546412d03dabd947
Mathew Munoz <mathew.munoz@edosoft.es>
2017-12-19 15:56:32 +0000

    WIP setTimeComponent UI

--- frontEnd/src/app/set-time-company/set-time-company.component.ts
+++ frontEnd/src/app/set-time-company/set-time-company.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-set-time-company',
@@ -10,6 +11,18 @@ export class SetTimeCompanyComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFormSubmit(companyTimeTrackerForm: NgForm) {
+    console.log(companyTimeTrackerForm.value);
+    console.log('checkinmin:' + companyTimeTrackerForm.controls['checkin-min'].value);
+    console.log('checkinmax:' + companyTimeTrackerForm.controls['checkin-max'].value);
+    console.log('checkoutmin:' + companyTimeTrackerForm.controls['checkout-min'].value);
+    console.log('checkoutmax:' + companyTimeTrackerForm.controls['checkout-max'].value);
+    console.log('checkoutfriday:' + companyTimeTrackerForm.controls['checkout-min-friday'].value);
+    console.log('checkoutmaxfriday:' + companyTimeTrackerForm.controls['checkout-max-friday'].value);
+    console.log('Form Valid:' + companyTimeTrackerForm.valid);
+    console.log('Form Submitted:' + companyTimeTrackerForm.submitted);
   }
 
 }
