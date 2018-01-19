697582679b67149848ce56cbc77ae5c46193b2b2
Daniel San Juan Abrante <Daniel San>
2018-01-19 15:51:13 +0000

    Added more time to get ip

--- frontEnd/src/app/check/check.component.ts
+++ frontEnd/src/app/check/check.component.ts
@@ -138,7 +138,7 @@ export class CheckComponent implements OnInit {
               break;
             }
           });
-        }, 700);
+        }, 1000);
       }
 
       getIp(){
@@ -188,7 +188,7 @@ export class CheckComponent implements OnInit {
                       break;
                 }
               });
-            }, 700);
+            }, 1000);
               this.readyCheckOut = false;
             }else{
               this.toastr.error('You should wait 5 minute to do checkout', 'Oops!');
